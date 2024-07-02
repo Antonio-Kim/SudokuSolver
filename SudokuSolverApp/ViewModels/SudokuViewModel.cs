a921f991369214e912dde49a92d33d772e896419
Antonio Kim <antoniok@sfu.ca>
2024-07-02 12:35:32 -0700

    Updating Sudoku ViewModel

--- SudokuSolverApp/ViewModels/SudokuViewModel.cs
+++ SudokuSolverApp/ViewModels/SudokuViewModel.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SudokuSolver.Solver.Models;
+using System.Collections.ObjectModel;
 
-namespace SudokuSolverApp.ViewModels
+namespace SudokuSolverApp.SudokuSolverApp.ViewModels;
+
+public class SudokuViewMdoel : ObservableObject
 {
-    internal class SudokuViewModel
+    private readonly GridService _gridService;
+    private SudokuSolver.Solver.Models.Grid _grid;
+
+    public SudokuViewMdoel()
     {
+        _gridService = new GridService();
+        _grid = _gridService.GetTable();
+    }
+
+    private int?[][] _table;
+    public int?[][] Table
+    {
+        get { return _table; }
+        set { SetProperty(ref _table, value); }
+    }
+
+    private void RefreshTable()
+    {
+        Table = _gridService.GetTable()?.table;
+    }
+
+    private void SolveTable()
+    {
+        _gridService.SolveTable();
+        RefreshTable();
+    }
+
+    public void UpdateCell(int row, int col, int num)
+    {
+        _gridService.UpdateCell(row, col, num);
+        RefreshTable();
+    }
+
+    public void DeleteCell(int row, int col)
+    {
+        _gridService?.DeleteCell(row, col);
+        RefreshTable();
     }
 }
